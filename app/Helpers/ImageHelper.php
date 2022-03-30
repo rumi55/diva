0e23eb0bdda020756fe26271939f97b044b98a3e
teiuqpeek <gurdin.k@gmail.com>
2022-03-30 18:55:03 +0300

    11

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -3,9 +3,6 @@
 namespace App\Helpers;
 
 // use Intervention\Image\Facades\Image;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Nette\Utils\Image;
 
 class ImageHelper
@@ -38,8 +35,6 @@ class ImageHelper
         return false;
     }
 
-    
-
     public static function thumb($filename, $width, $height, $ext='webp')
     {
         if (!$filename) {
@@ -55,68 +50,12 @@ class ImageHelper
             $name = $pathInfo['filename'] . '-' . $width . '-' .$height. '.' . $ext;
             $path = public_path(static::$storePath . $name);
             $image = Image::make($originPath)->encode($ext, 90);
-             $image->resize($width, $height, function($const) {
-                $const->aspectRatio();
-            })->crop($width, $height)->save($path);
-            //  $image->resize($width, $height, function($const) {
-            //     $const->aspectRatio();
-            // })->save($path);
-            
-            // $image->crop($width, $height)->save($path);
-            return asset(static::$storePath . $name);
-        }
-
-        return null;
-    }
-    public static function thumbResp($filename, $width, $height, $ext='webp')
-    {
-        if (!$filename) {
-            return null;
-        }
-
-        if ($image = static::getCachePath($filename, $width, $height, $ext)) {
-            return $image;
-        }
-
-        if ($originPath = static::getImagePath($filename)) {
-            $pathInfo = pathinfo($originPath);
-            $name = $pathInfo['filename'] . '-' . $width . '-' .$height. '.' . $ext;
-            $path = public_path(static::$storePath . $name);
-            $image = Image::make($originPath)->encode($ext, 90);
-            
-             $image->resize($width, $height, function($const) {
+            $image->resize($width, $height, function($const) {
                 $const->aspectRatio();
             })->save($path);
-            
-            // $image->crop($width, $height)->save($path);
             return asset(static::$storePath . $name);
         }
 
         return null;
     }
-    public static function thumbFit($filename, $width, $height, $ext='webp')
-    {
-        if (!$filename) {
-            return null;
-        }
-
-        if ($image = static::getCachePath($filename, $width, $height, $ext)) {
-            return $image;
-        }
-
-        if ($originPath = static::getImagePath($filename)) {
-            $pathInfo = pathinfo($originPath);
-            $name = $pathInfo['filename'] . '-' . $width . '-' .$height. '.' . $ext;
-            $path = public_path(static::$storePath . $name);
-            $image = Image::make($originPath)->encode($ext, 90);
-            
-            
-            $image->fit($width, $height)->save($path);
-            return asset(static::$storePath . $name);
-        }
-
-        return null;
-    }
-    
-
 }
