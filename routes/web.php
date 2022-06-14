37cd05c9c0a7a00edbd0060faba90a31a95bdda5
teiuqpeek <gurdin.k@gmail.com>
2022-06-14 14:15:03 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,7 @@ use App\Http\Controllers\SitemapXmlController;
 */
 
 Route::get('/', [PostsController::class, 'mainpage']);
-// Технические работы
-Route::get('/', function(){
-    return redirect('404');
-});
+
 Route::get('/sitemap.xml', [SitemapXmlController::class, 'index']);
 
 
